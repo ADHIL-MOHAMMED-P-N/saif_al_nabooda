5a60d56cc735e969c997bcc557821fca9739d426
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-02-07 23:46:02 +0530

    Js: navbar toggling on click mobilelinks

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 const hamburgerBtn = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobile-menu");
+const mobileLinks = document.querySelectorAll(".mobile__link");
 
 /* toggling hamburger button */
 hamburgerBtn.addEventListener("click", () => {
   hamburgerBtn.classList.toggle("menu-open");
   mobileMenu.classList.toggle("hidden");
+});
+
+/* closing mobile navbar and handburger menu on clicking mobile navbar links */
+
+mobileLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburgerBtn.classList.toggle("menu-open");
+    mobileMenu.classList.toggle("hidden");
+  });
 });
