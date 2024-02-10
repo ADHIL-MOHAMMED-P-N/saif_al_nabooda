0decb7b359cc3b86a06affe8346928ec85880c6e
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-02-10 15:32:39 +0530

    Buffet set complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: [
+    "./*.html",
+    "./products/*.html",
+  ] /* update content according to file structure */,
   theme: {
     fontFamily: {
       bebasNeue: ["Bebas Neue", "sans-serif"],
