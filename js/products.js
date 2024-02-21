9c5ff20e95ea62ad6496558b293453e8536132f7
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2024-02-21 23:46:18 +0530

    product page animation

--- js/products.js
+++ js/products.js
@@ -2,6 +2,12 @@ const hamburgerBtn = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileLinks = document.querySelectorAll(".mobile__link");
 
+/* ano animation */
+AOS.init({
+  offset: 100,
+  duration: 1000,
+});
+
 /* toggling hamburger button */
 hamburgerBtn.addEventListener("click", () => {
   hamburgerBtn.classList.toggle("menu-open");
